846e5dcd3c31f2fd0f4b5fa4516897abd6a6826c
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-21 10:51:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,26 @@
 # require modules here
+require 'yaml'
 
-def load_library
+def load_library(file_path)
   # code goes here
+  library = {}
+  YAML.load_file(file_path).each do |key, value|
+    library[key] = {}
+    library[key][:english] = value[0]
+    library[key][:japanese] = value[1]
+end
+library
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, "emoticon")
   # code goes here
+  library = load_library(file_path)
+  result = library[emoticon][1]
+    if result
+      result
+  else
+    "Sorry, that emoticon was not found."
+  end
 end
 
 def get_english_meaning
