0a361470ed1f4b3396fcf705c5427aafcb3623ee
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-21 11:13:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(file_path, emoticon)
     if result
       result
   else
-    "Sorry, that emoticon was not found."
+    return "Sorry, that emoticon was not found."
   end
 end
 
